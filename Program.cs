f171eb9ebcf8f6b3c1b2666e0c5c0e5f4831e658
eduardonunespp <carlospn200406@gmail.com>
2023-08-07 23:22:32 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Entre com o primeiro valor: ");
+            float value1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o segundo valor: ");
+            float value2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = value1 + value2;
+            
+            Console.WriteLine($"A soma dos resultados é {resultado}");
+
+
+
+        }
+    }
+}
